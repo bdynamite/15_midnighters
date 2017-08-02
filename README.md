86ed738fd0077b23637cff6503845728abfdcfed
bdynamite <bdynamite@yandex.ru>
2017-08-02 13:20:45 +0300

    первая попытка

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+This script displays devman's users whitch send their solutions after midnight
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+# Quick Start
+
+Example of script launch on Windows, Python 3.6:
+
+```#!bash
+
+$ python seek_dev_nighters.py
+
+# output example
+
+Following users send their solutions after midnight:
+"RamusikValery" 3 times
+"vergeev" 2 times
+...
+
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,45 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pprint
+import datetime
+import pytz
+import collections
+import tqdm
 
-def get_midnighters():
-    pass
+
+def load_attempts():
+    pages = 10
+    url = 'https://devman.org/api/challenges/solution_attempts/?page={}'
+    records_list = []
+    for page in tqdm.tqdm(range(1, pages + 1), desc='collecting data'):
+        responce = requests.get(url=url.format(page))
+        records_list.extend(responce.json()['records'])
+    return records_list
+
+
+def convert_data(data_list):
+    converted_list = []
+    for element in data_list:
+        if not element['timestamp']:
+            continue
+        time = datetime.datetime.fromtimestamp(element['timestamp'], pytz.timezone(element['timezone']))
+        converted_list.append((element['username'], time))
+    return converted_list
+
+
+def get_midnighters(data_list):
+    midnighters = collections.Counter([x[0] for x in data_list if x[1].hour in range(5)])
+    return midnighters
+
+
+def print_midnighters(midnighters):
+    print('Following users send their solutions after midnight:')
+    print('\n'.join(['"{}" {} times'.format(x[0], x[1])
+                     for x in sorted(midnighters.items(), key=lambda y: y[1], reverse=True)]))
 
 if __name__ == '__main__':
-  pass
+    api_data = load_attempts()
+    data_with_time = convert_data(api_data)
+    pprint.pprint(data_with_time)
+    midnighters = get_midnighters(data_with_time)
+    print_midnighters(midnighters)
+
